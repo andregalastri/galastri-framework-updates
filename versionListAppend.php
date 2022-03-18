e67b0239be9f35bcf8455dd36b57529d1e8545a1
andregalastri <contato@andregalastri.com.br>
2022-03-18 18:12:22 +0000

    versionListAppend script now runs from anywhere.

--- versionListAppend.php
+++ versionListAppend.php
@@ -5,7 +5,7 @@ $versionList = include($versionListFile);
 if(array_search($argv[1], $versionList) === false) {
     array_push($versionList, $argv[1]);
 
-    $fp = fopen($versionListFile, 'w+');
+    $fp = fopen(__DIR__.'/'.$versionListFile, 'w+');
     fwrite($fp, "<?php\n\n");
     fwrite($fp, "return ".var_export($versionList, true).";");
     fclose($fp);
